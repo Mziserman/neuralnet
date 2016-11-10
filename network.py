08e65cf747c473559a2c8b42c65e1745301f67d5
zarmi <martinziserman@gmail.com>
2016-11-10 10:17:46 +0000

    not working backward propagation

--- network.py
+++ network.py
@@ -3,6 +3,8 @@ import random
 from neuron import Neuron
 from layer import Layer
 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 class Network:
     def __init__(self, shape):
@@ -24,3 +26,66 @@ class Network:
         for layer in self.layers[1:]:
             for neuron in layer.neurons:
                 neuron.feed_forward(neuron.data)
+                
+        ##for neuron in self.layers[-1].neurons:
+            ##print(neuron.sigmoid(neuron.data))
+            
+    def back_propagate(self, targets, N, M):
+        if len(targets) != len(self.layers[-1].neurons):
+            raise ValueError('wrong number of target values')
+
+        # calculate error terms for output
+        i = 0
+        for neuron in self.layers[-1].neurons:
+            error = targets[i] - neuron.sigmoid(neuron.data)
+            neuron.delta = neuron.derivative_sigmoid(neuron.sigmoid(neuron.data)) * error
+            i += 1
+
+        # calculate error terms for hiddens
+        for i in range(len(self.layers)):
+            for neuron in self.layers[-(i+1)].neurons:
+                error = 0
+                for connection in neuron.connections["backward"]:
+                    error = error + neuron.delta * connection["weight"]
+                for connection in neuron.connections["backward"]:
+                    connection["neuron"].delta = neuron.derivative_sigmoid(neuron.sigmoid(neuron.data)) * error
+        
+        for i in range(len(self.layers)):
+            for neuron in self.layers[-(i+1)].neurons:
+                for connection in neuron.connections["backward"]:
+                    change = neuron.delta * neuron.sigmoid(connection["neuron"].data)
+                    new_weight = connection["weight"] + N * change + M * neuron.change
+                    connection["weight"] = new_weight
+                    neuron.change = change
+                    print(new_weight)
+                
+                    for connection in connection["neuron"].connections["forward"]:
+                        if connection["neuron"] == neuron:
+                            connection["weight"] = new_weight
+                
+        # calculate error
+        error = 0.0
+        for k in range(len(targets)):
+            error = error + 0.5*(targets[k] - sigmoid(self.layers[-1].neurons[k].data))**2
+        
+        self.remove_data()
+        
+        return error
+    
+    def remove_data(self):
+        for layer in self.layers:
+            for neuron in layer.neurons:
+                neuron.data = 0
+    
+    def train(self, patterns, iterations=1000, N=0.5, M=0.1):
+        # N: learning rate
+        # M: momentum factor
+        for i in range(iterations):
+            error = 0.0
+            for p in patterns:
+                inputs = p[0]
+                targets = p[1]
+                self.feed_forward(inputs)
+                error = error + self.back_propagate(targets, N, M)
+            if i % 100 == 0:
+                print('error %-.5f' % error)
--- neuron.py
+++ neuron.py
@@ -8,27 +8,32 @@ class Neuron:
             "backward": []
         }
         self.data = 0
+        self.change = 0
 
     def __str__(self):
         return "data: {0}, forward connections: {1}, backward connection: {2}".format(self.data, len(self.connections["forward"]), len(self.connections["backward"]))
         
     def connect(self, layer):
         for neuron in layer.neurons:
+            weight = random.uniform(-1, 1)
             self.connections["forward"].append({
                 "neuron": neuron,
-                "weight": random.uniform(-1, 1)
+                "weight": weight
             })
             neuron.connections["backward"].append({
-                "neuron": self
+                "neuron": self,
+                "weight": weight
             })
 
     def feed_forward(self, input):
         for connection in self.connections["forward"]:
-            forward_data = input * connection["weight"]
+            forward_data = self.sigmoid(input * connection["weight"])
             ##backward_connection = next(x for x in connection["neuron"].connections["backward"] if self == x["neuron"])
             connection["neuron"].data += forward_data
 
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
         
+    def derivative_sigmoid(self, x):
+        return x * (1 - x)
     
